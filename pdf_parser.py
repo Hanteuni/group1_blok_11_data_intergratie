763d1fae2c651bda780472877b0e8926294d10e9
Hanteuni <HMI.Teunissen@student.han.nl>
2021-06-09 16:24:21 +0200

    Code pep8 gemaakt

--- pdf_parser.py
+++ pdf_parser.py
@@ -2,41 +2,49 @@ from tika import parser
 import json
 
 
-def file_reader(fname, json_file):
+def file_reader(fname, json_file_annotation):
+    """Reads in the pdf file using the tika libary, and converts it to a .json structered dictionairy
+    :param fname: path to the PGCP person pdf
+    :param json_file_annotation: .JSON formatted dictionary
+    :return: .json formatted dictionary containing the pdf data
+    """
     parsed_pdf = parser.from_file(fname)
     actual_data = []
     data = parsed_pdf['content']
     temp_ls = data.split("\n")
     for item in temp_ls:
+        # checks wether the pdf item isn't empty
         if item != "":
             actual_data.append(item)
     profile_feature = actual_data[2].split(" ")
     conditions_and_symptoms = actual_data[5].split(" ")
-    json_file[profile_feature[0]] = {
+    json_file_annotation[profile_feature[0]] = {
         "Profile": {
-          "birth_year": profile_feature[2],
-          "birth_month": profile_feature[1],
-          "Sex": profile_feature[3],
-          "Ethnicity": profile_feature[4]
+            "birth_year": profile_feature[2],
+            "birth_month": profile_feature[1],
+            "Sex": profile_feature[3],
+            "Ethnicity": profile_feature[4]
         },
-      "Conditions_or_symptoms": conditions_and_symptoms[1:]
-      }
+        "Conditions_or_symptoms": conditions_and_symptoms[1:]
+    }
 
     return json_file
 
 
-def file_to_json(json_file):
+def file_to_json(json_file_dict):
+    """Write the .json formatted dictonary to a .json file.
+    :param json_file_dict: .json formatted dictonairy
+    :return: a local .json file
+    """
     with open("data_intergratie.json", "w+") as new_json:
         print("uploading the jason file... ")
-        json.dump(json_file, new_json)
+        json.dump(json_file_dict, new_json)
         print("file is done")
-    print(json_file)
 
 
 if __name__ == '__main__':
     json_file = {}
-    file_names = ["PGPC-1.pdf","PGPC-11.pdf","PGPC-21.pdf"]
+    file_names = ["PGPC-1.pdf", "PGPC-11.pdf", "PGPC-21.pdf"]
     for file_name in file_names:
-        json_file =file_reader(fname=file_name, json_file=json_file)
+        json_file = file_reader(fname=file_name, json_file_annotation=json_file)
     file_to_json(json_file)
-
--- ten_results.py
+++ ten_results.py
@@ -5,25 +5,34 @@ import tqdm
 import time
 import sys
 
-
 json_file = {}
 
 
 def file_reader(fname, variant_type):
-    counter = 0
+    """Reads and parses the variant file in the first on the first ten annotations.
+    On what it filters depends on the variant type as paramater, which is either frame_shift
+    or missense
+    :param fname: the path to the annotated filter variant file
+    :param variant_type: either missense or frame_shift
+    :return:
+    """
     type_dict = {}
     with open(fname, "r") as file:
         for line in file:
             variants = []
+            # len(type_dict) is there to ensure there are only 10 results
             if not line.startswith("#") and len(type_dict) < 10:
                 info = line.split("\t")
+                # info[7] contains the annotation string
                 info_split = info[7].split("|")
-                # print(info_split)
+                # checks which variant it has and goes through the annotation where the missense is
                 if variant_type == "missense_variant":
                     indices = [i for i, x in enumerate(info_split) if x == "missense_variant"]
                 if variant_type == "frame_shift_variant":
-                    indices = [i for i, x in enumerate(info_split) if x == "missense_variant"]
+                    indices = [i for i, x in enumerate(info_split) if x == "frame_shift_variant"]
+                # pares the info from the variant string
                 for index in indices:
+                    # the variant is always 8 indexes further from the wordt missense/frame_shift_variant
                     variant = info_split[index + 8]
                     variants.append(variant)
                 gene_name = info_split[3]
@@ -31,7 +40,15 @@ def file_reader(fname, variant_type):
     return type_dict
 
 
-def write_to_json(missense_dict, frame_shift_dict,missense_name_dict, frame_shift_name_dict, person):
+def write_to_json(missense_dict, frame_shift_dict, missense_name_dict, frame_shift_name_dict, person):
+    """Writes both the missense and frame_shift dictionaries to a single formatted .json files.
+    :param missense_dict: Dictionarie with all the missense variants from the filtered annotation file
+    :param frame_shift_dict: Dictionarie with all the frame_shift variants from the filtered annotation file
+    :param missense_name_dict: Dictonary containing all the HNGC names of the missense variants
+    :param frame_shift_name_dict: Dictonary containing all the HNGC names of the missense variants
+    :param person: To which PGCP person all the annotion belongs
+    :return: A dictionary that contains all 4 of the dictionaries
+    """
     json_file[person] = {
         "missense_variant": missense_dict,
         "missense_HGNC_name": missense_name_dict,
@@ -40,15 +57,23 @@ def write_to_json(missense_dict, frame_shift_dict,missense_name_dict, frame_shif
 
 
 def json_writer():
-    with open("data_intergratie_variants.json", "w+") as new_json:
+    """"Writes the json dictonary that was created in the write_to_json fuction to a local file
+    """
+    with open("data_intergratie_variants_test.json", "w+") as new_json:
         print("uploading the jason file... ")
         json.dump(json_file, new_json)
         print("file is done")
 
 
-def get_HGNC_name(dict,name=""):
+def get_HGNC_name(variant_dict):
+    """ For every variant name, the full out name of the variant is retrieved from the HNGC database.
+    If a name is not in the HNGC database it will return a NO HNGC NAME as name
+    :param variant_dict: The dictonary created in file_reader containing
+    :return: a dictonary with the HNGC names
+    """
     name_dict = {}
-    for variant in tqdm.tqdm(dict, total=len(dict), desc="Retrieving names from HGNC"):
+    for variant in tqdm.tqdm(variant_dict, total=len(variant_dict), desc="Retrieving names from HGNC"):
+        # a time.sleep to prevent from getting kicked from HNGC server
         time.sleep(3)
         headers = {
             'Accept': 'application/json',
@@ -64,12 +89,10 @@ def get_HGNC_name(dict,name=""):
             method,
             body,
             headers)
-        #
+        # Making sure there isn't any connection issues
         if response['status'] == '200':
             data = json.loads(content)
             try:
-                # print(
-                #     'Name:' + data['response']['docs'][0]['name'])
                 name_dict[variant] = data['response']['docs'][0]['name']
             except IndexError:
                 name_dict[variant] = "NO HNGC NAME"
@@ -81,16 +104,18 @@ def get_HGNC_name(dict,name=""):
 def main(missense_file, frame_shift_file, person):
     missense_dict = file_reader(fname=missense_file, variant_type="missense_variant")
     frame_shift_dict = file_reader(fname=frame_shift_file, variant_type="frame_shift_variant")
-    missense_name_dict = get_HGNC_name(missense_dict,name="missense")
-    frame_shift_name_dict = get_HGNC_name(frame_shift_dict,name="frame_shift")
-    write_to_json(missense_dict,frame_shift_dict,missense_name_dict, frame_shift_name_dict, person=person)
-    json_writer(json_file_name)
+    missense_name_dict = get_HGNC_name(missense_dict)
+    frame_shift_name_dict = get_HGNC_name(frame_shift_dict)
+    write_to_json(missense_dict, frame_shift_dict, missense_name_dict, frame_shift_name_dict, person=person)
+    json_writer()
 
 
 if __name__ == '__main__':
+    """
+    System argv: missense annation filepath, frame shift annotion filepath, json_key/PGCP-person name
+    """
     if len(sys.argv) < 4:
         print("missing arguments; need missense annotation; frame_shift annotation and person\n"
               "exiting now...")
         exit()
-    main(sys.argv[1],sys.argv[2],sys.argv[3])
-
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
